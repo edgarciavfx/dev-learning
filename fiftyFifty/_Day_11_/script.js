eb84731d8dec59308c771b7defa5b3e93e4716fc
Edgar Garcia <edgarciavfx@gmail.com>
2021-05-17 13:23:38 -0500

    fiftyInFifty
    added the daily proyects from udemy course by Brad Traversy.

---
+++ fiftyFifty/_Day_11_/script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (eve) => {
+  insert.innerHTML = `
+  <div class="key">
+    ${eve.key === ' ' ? 'Space' : eve.key}
+    <small>event.key</small>
+  </div>
+
+  <div class="key">
+    ${eve.keyCode}
+    <small>event.keyCode</small>
+  </div>
+
+  <div class="key">
+  ${eve.code}
+    <small>event.code</small>
+  </div>
+  `
+});
